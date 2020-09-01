2a022e4e6c683a3951e7bbf54fcc8e895b266071
leandro alfonso <leamanya24.6@gmail.com>
2020-09-01 20:07:53 -0300

    Agrego EJ1

---
+++ src/Library/Alfajor.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Expert_SRP
+{
+    public class Alfajor 
+    {   
+        //Esta bien
+        public Alfajor(double precioMasa, double precioDulce)
+        {
+            this.PrecioDulce = precioDulce;
+            this.PrecioMasa = precioMasa;
+        }
+        public Double PrecioDulce {get;set;}
+        public Double PrecioMasa {get; set;}
+    }
+}
---
+++ src/Library/Kiosco.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace Expert_SRP
+{
+    public class Kiosco 
+    {
+        public Boolean PuedeComprar(Alfajor a, Double dinero, String moneda) 
+        {
+            Double pesos = ConvertirAPesos(dinero, moneda);
+            return pesos >= a.PrecioDulce + a.PrecioMasa;
+        }
+
+        //Deberia haber una clase que convierta monedas
+        //Ya que si por ejemplo el dia de mañana las conversion cambiaran, no tenemos que
+        //escribir directamente sobre la clase kiosko
+        private Double ConvertirAPesos(Double dinero, String moneda)
+        {
+            if (moneda.Equals("U$S")) 
+            {
+                return dinero / 30;
+            } 
+            else if (moneda.Equals("$")) 
+            {
+                return dinero;
+            }
+            else 
+            {
+                return -1;
+            }
+        }
+    }
+}
